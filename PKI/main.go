bf9ce5efbae6399e2e26874588005ea2ad826fbf
ocdal <alexander.bjornsrud@orangecyberdefense.com>
2022-05-11 09:25:01 +0200

    .

--- PKI/main.go
+++ PKI/main.go
@@ -1,17 +1,16 @@
 package pki
 
 import (
-	"fmt"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"log"
+	"fmt"
+	"github.com/sirupsen/logrus"
 	"math/big"
 	"os"
 	"time"
-	log "github.com/sirupsen/logrus"
 )
 
 type PKI struct {
@@ -25,62 +24,76 @@ type PKI struct {
 	code string
 }
 
-func NewPKI(org string, addr string, code string)(*PKI,error){
+func NewPKI(org string, addr string, code string) (*PKI, error) {
 	var err error
 
-	out := &PKI{org:org,addr:addr,code:code}
+	out := &PKI{org: org, addr: addr, code: code}
 	out.cert = &x509.Certificate{
-		SerialNumber: big.NewInt(1653),
-		Subject: Pkix(),
-		NotBefore: time.Now(),
-		NotAfter: time.Now().AddDate(10,0,0),
-		IsCA: true,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth,x509.ExtKeyUsageServerAuth},
-		KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		SerialNumber:          big.NewInt(1653),
+		Subject:               Pkix(),
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 		BasicConstraintsValid: true,
 	}
 
-    out.priv, err = rsa.GenerateKey(rand.Reader, 2048)
+	out.priv, err = rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		logrus.Errorf("Could not Create PKI")
 		return out, err
 	}
 
 	out.pub = &out.priv.PublicKey
-	out.ca,err = x509.CreateCertificate(rand.Reader,out.cert,out.cert,out.pub,out.priv)
+
+	out.ca, err := x509.CreateCertificate(
+		rand.Reader,
+		out.cert,
+		out.cert,
+		out.pub,
+		out.priv)
+
 	if err != nil {
 		logrus.Errorf("Could not Create Cert")
 		return out, err
 	}
 
 	return out, nil
-	
+
 }
 
-func (pki *PKI)Pkix(){
+func (pki *PKI) Pkix() {
 	pki.name = pkix.Name{
-		Organization: []string{pki.org},
-		Country: []string{"NO"},
-		Province: []string{"Oslo"},
-		Locality: []string{"Oslo"},
+		Organization:  []string{pki.org},
+		Country:       []string{"NO"},
+		Province:      []string{"Oslo"},
+		Locality:      []string{"Oslo"},
 		StreetAddress: []string{pku.addr},
-		PostalCode: []string{pki.code},
+		PostalCode:    []string{pki.code},
 	}
 }
 
-func (pki *PKI)Save() error {
-	out, err := os.Create("ca.crt")
-	if(err!=nil){return err}
-	pem.Encode(out,&pem.Block{Type:"CERTIFICATE",Bytes:x509.MarshallPKCS1PrivateKey(pki.ca)})
+func (pki *PKI) Save(path string) error {
+	out, err := os.Create(fmt.Sprintf("%s/%s", path, "cert.pem"))
+	if err != nil {
+		return err
+	}
+	pem.Encode(out, &pem.Block{Type: "CERTIFICATE", Bytes: x509.MarshallPKCS1PrivateKey(pki.ca)})
 	out.Close()
 
-	out, err := os.Create("ca.key", os.O_WERONLY|os.O_CREATE|os.O_TRUNK,0600)
-	if(err!=nil){return err}
-	pem.Encode(out,&pem.Block{Type:"RSA PRIVATE KEY",Bytes:x509.MarshallPKCS1PrivateKey(pki.priv)})
+	out, err = os.Create(fmt.Sprintf("%s/%s", path, "key.pem"), os.O_WERONLY|os.O_CREATE|os.O_TRUNK, 0600)
+	if err != nil {
+		return err
+	}
+	pem.Encode(out, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshallPKCS1PrivateKey(pki.priv)})
 	out.Close()
 
-	out, err := os.Create("ca.pub", os.O_WERONLY|os.O_CREATE|os.O_TRUNK,0600)
-	if(err!=nil){return err}
+	out, err = os.Create(fmt.Sprintf("%s/%s", path, "pub.pem"), os.O_WERONLY|os.O_CREATE|os.O_TRUNK, 0600)
+	if err != nil {
+		return err
+	}
+
 	out.Write(pki.pub)
 	out.Close()
 }
