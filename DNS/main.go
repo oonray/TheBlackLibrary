cf4c7c96ced3417b7003ac18c9442323ea3e957d
oonray <alexanderbjornsrud@gmail.com>
2022-11-08 16:23:49 +0100

    .

--- DNS/main.go
+++ DNS/main.go
@@ -1,39 +1,84 @@
 package main
 
 import (
+	//	"flag"
+	"flag"
+	"fmt"
 	"github.com/miekg/dns"
 	"log"
 	"net"
+	"strings"
 )
 
-var mp []*Map
+var (
+	hsts  Hosts = Hosts{Hosts: map[string]string{}}
+	host  string
+	port  string
+	udp   bool
+	proto string = "tcp"
+)
 
-type Map struct {
-	Handler  func(w dns.ResponseWriter, req *dns.Msg)
-	Ip       string
-	Hostname string
+type Hosts struct {
+	Hosts map[string]string
 }
 
-func (m *Map) default_handler(w dns.ResponseWriter, req *dns.Msg) {
-	var rsp dns.Msg
-	rsp.SetReply(req)
+func (h *Hosts) Set(value string) error {
+	data := strings.Split(value, ":")
+	log.Printf("%v", data)
+	h.Hosts[data[0]] = data[1]
+	log.Printf("%v", h.Hosts)
+	return nil
+}
 
-	for _, q := range req.Question {
-		a := dns.A{
-			Hdr: dns.RR_Header{
-				Name:   q.Name,
-				Rrtype: dns.TypeA,
-				Class:  dns.ClassINET,
-				Ttl:    0,
-			},
-			A: net.ParseIP(m.Ip).To4(),
+func default_handler(w dns.ResponseWriter, req *dns.Msg) {
+	var a dns.Msg = dns.Msg{}
+	a.SetReply(req)
+
+	for _, data := range req.Question {
+		out, in := hsts.Hosts[strings.Trim(data.Name, ".")]
+		if in {
+			log.Printf("found %s", data.Name)
+			answer := dns.A{
+				Hdr: dns.RR_Header{
+					Name:   data.Name,
+					Rrtype: dns.TypeA,
+					Class:  dns.ClassINET,
+					Ttl:    0,
+				},
+				A: net.ParseIP(out).To4(),
+			}
+			a.Answer = append(a.Answer, &answer)
 		}
-		resp.Answer = append(resp.Answer, &a)
+		if !in {
+			log.Printf("not found %s", data.Name)
+			ip, err := net.ResolveIPAddr("ip", data.Name)
+			if err == nil {
+				answer := dns.A{
+					Hdr: dns.RR_Header{
+						Name:   data.Name,
+						Rrtype: dns.TypeA,
+						Class:  dns.ClassINET,
+						Ttl:    0,
+					},
+					A: ip.IP,
+				}
+				a.Answer = append(a.Answer, &answer)
+			}
+		}
 	}
-	w.WriteMsg(&rsp)
+	w.WriteMsg(&a)
+}
+
+func init() {
+	flag.StringVar(&host, "H", "127.0.0.1", "The host to bind the server to")
+	flag.StringVar(&port, "P", "53", "The port to bind the server to")
+	flag.Parse()
 }
 
 func main() {
-	dns.HandleFunc(".", handle)
-	log.Fatal(dns.ListenAndServe(":53", "udp", nil))
+	for _, data := range flag.Args() {
+		hsts.Set(data)
+	}
+	dns.HandleFunc(".", default_handler)
+	log.Fatal(dns.ListenAndServe(fmt.Sprintf("%s:%s", host, port), "udp", nil))
 }
